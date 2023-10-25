5a30fbda6eae3995c3640a8978d7ef925a659b59
Fagor <oleg-veber@mail.ru>
2023-10-25 16:36:39 +0700

    change route

--- src/main/java/ru/nsu/fit/directors/userservice/configuration/SecurityConfiguration.java
+++ src/main/java/ru/nsu/fit/directors/userservice/configuration/SecurityConfiguration.java
@@ -16,7 +16,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable)
-            .authorizeHttpRequests(matcher -> matcher.requestMatchers("/user/login", "/user/register", "/business/swagger-ui/**", "/business/api-docs/**")
+            .authorizeHttpRequests(matcher -> matcher.requestMatchers("/user/login", "/user/register", "/user/swagger-ui/**", "/user/api-docs/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
