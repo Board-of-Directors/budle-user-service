e7ff67eae53bf2a04bf91be662fb206a4a89384e
Fagor <oleg-veber@mail.ru>
2023-10-31 16:33:11 +0700

    resolve cors policy

---
+++ src/main/java/ru/nsu/fit/directors/userservice/configuration/WebMvcConfiguration.java
@@ -0,0 +1,22 @@
+package ru.nsu.fit.directors.userservice.configuration;
+
+import jakarta.annotation.Nonnull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguration {
+    @Bean
+    @Primary
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@Nonnull CorsRegistry registry) {
+                registry.addMapping("/**").allowedOriginPatterns("*").allowCredentials(true);
+            }
+        };
+    }
+}
