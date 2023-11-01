6171a0e4f03847281888abc015e38ea97070a70c
Fagor <oleg-veber@mail.ru>
2023-11-01 11:34:58 +0700

    enable web security

--- src/main/java/ru/nsu/fit/directors/userservice/configuration/SecurityConfiguration.java
+++ src/main/java/ru/nsu/fit/directors/userservice/configuration/SecurityConfiguration.java
@@ -3,25 +3,22 @@ package ru.nsu.fit.directors.userservice.configuration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.cors(configurer -> configurer.configurationSource(corsConfigurationSource()))
-            .csrf(AbstractHttpConfigurer::disable)
+        return http.csrf(AbstractHttpConfigurer::disable)
             /*.authorizeHttpRequests(matcher -> matcher
                 .requestMatchers("/user/login", "/user/register", "/user/swagger-ui/**", "/user/api-docs/**")
                 .permitAll()
@@ -31,24 +28,10 @@ public class SecurityConfiguration {
             )
 
              */
-            .cors(configurer -> configurer.configurationSource(corsConfigurationSource()))
+            .cors(Customizer.withDefaults())
             .build();
     }
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("*"));
-        configuration.setAllowedOrigins(List.of("http://localhost:10888"));
-        configuration.setAllowedMethods(List.of(CorsConfiguration.ALL));
-        configuration.setExposedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true);
-        configuration.setAllowedHeaders(List.of("*"));
-        configuration.setMaxAge(3600L);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
