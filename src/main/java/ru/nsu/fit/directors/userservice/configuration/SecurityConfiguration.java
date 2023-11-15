f1120381fba99340a875b9431fa5022defc35e44
Fagor <oleg-veber@mail.ru>
2023-11-15 12:08:41 +0700

    Return security configuration

--- src/main/java/ru/nsu/fit/directors/userservice/configuration/SecurityConfiguration.java
+++ src/main/java/ru/nsu/fit/directors/userservice/configuration/SecurityConfiguration.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,18 +16,12 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable)
-            .authorizeHttpRequests(matcher -> matcher.anyRequest().permitAll())
-            /*.authorizeHttpRequests(matcher -> matcher
+            .authorizeHttpRequests(matcher -> matcher
                 .requestMatchers("/user/login", "/user/register", "/user/swagger-ui/**", "/user/api-docs/**")
                 .permitAll()
-                .requestMatchers(HttpMethod.OPTIONS).permitAll()
                 .anyRequest()
                 .authenticated()
             )
-
-             */
-            // .cors()
-            //.and()
             .build();
     }
 
