e9ff723ee8e40ad4a503c8b93a119482f5661e9c
Fagorym <oleg-veber@mail.ru>
2024-04-03 22:58:41 +0700

    Add echo ruchka

--- src/main/java/ru/nsu/fit/directors/userservice/controller/ChatController.java
+++ src/main/java/ru/nsu/fit/directors/userservice/controller/ChatController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import ru.nsu.fit.directors.userservice.dto.request.ChatMessage;
 import ru.nsu.fit.directors.userservice.service.ChatService;
@@ -19,5 +20,12 @@ public class ChatController {
     public void send(@DestinationVariable Long orderId, Message<ChatMessage> message) {
         log.info("received order message {}", orderId);
         chatService.save(message.getPayload(), orderId);
+    }
+
+    @MessageMapping("/send")
+    @SendTo("/topic/greetings")
+    public String greeting(Message<String> message) {
+        log.info("Received messsage {}", message);
+        return "Hello ololo " + message;
     }
 }
