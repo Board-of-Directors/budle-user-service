f5a9cce55994b9acd9e409e5bce044e5c78673fb
Fagorym <oleg-veber@mail.ru>
2024-04-03 22:38:15 +0700

    Set user destination prefix

--- src/main/java/ru/nsu/fit/directors/userservice/configuration/WebSocketConfiguration.java
+++ src/main/java/ru/nsu/fit/directors/userservice/configuration/WebSocketConfiguration.java
@@ -14,6 +14,7 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
         config.setApplicationDestinationPrefixes("/user");
+        config.setUserDestinationPrefix("/user");
     }
 
     @Override
