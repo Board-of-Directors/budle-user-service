46ce3d96194a746340866f6499b86fcc3e0faeff
Fagorym <oleg-veber@mail.ru>
2024-04-04 08:46:13 +0700

    Add senderType

--- src/main/java/ru/nsu/fit/directors/userservice/dto/MessageDto.java
+++ src/main/java/ru/nsu/fit/directors/userservice/dto/MessageDto.java
@@ -2,5 +2,5 @@ package ru.nsu.fit.directors.userservice.dto;
 
 import java.time.LocalDateTime;
 
-public record MessageDto(String message, LocalDateTime timestamp) {
+public record MessageDto(String message, LocalDateTime timestamp, String senderType) {
 }
