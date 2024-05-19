9acaa49224b8e492d64f2e7c25c2bfd8de24e981
Fagorym <oleg-veber@mail.ru>
2024-05-20 01:07:53 +0700

    Delete imports

--- src/main/java/ru/nsu/fit/directors/userservice/service/FavouritesServiceImpl.java
+++ src/main/java/ru/nsu/fit/directors/userservice/service/FavouritesServiceImpl.java
@@ -16,7 +16,6 @@ import ru.nsu.fit.directors.userservice.repository.UserRepository;
 import javax.annotation.Nonnull;
 import javax.annotation.ParametersAreNonnullByDefault;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
